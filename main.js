6ebbcbf2169e2139e8cfaf65b2defd6e052cdf6b
Ponkina1 <paulitas265@gmail.com>
2024-11-20 00:31:53 -0500

    recoge10

--- main.js
+++ main.js
@@ -51,32 +51,28 @@ let playerOnFloor = false;
 const keyStates = {};
 const collectedItems = [];
 
-// Crear varios objetos recolectables manualmente
-const collectible1 = collectibleObject.clone();
-collectible1.position.set(-2, 1, -4);
-collectible1.name = "Collectible1";
-scene.add(collectible1);
+// Crear geometría básica (un dodecaedro) en lugar de la torre
+const geometry = new THREE.DodecahedronGeometry(0.5);
+const material = new THREE.MeshPhongMaterial({ 
+  color: 0x00ff00,
+  shininess: 100
+});
 
-const collectible2 = collectibleObject.clone();
-collectible2.position.set(3, 1.5, -8);
-collectible2.name = "Collectible2";
-scene.add(collectible2);
+const geometry1 = new THREE.DodecahedronGeometry(0.5);
+const material1 = new THREE.MeshPhongMaterial({ 
+  color: 0x00ff00,
+  shininess: 100
+});
 
-const collectible3 = collectibleObject.clone();
-collectible3.position.set(1, 0.8, -3);
-collectible3.name = "Collectible3";
-scene.add(collectible3);
+const collectibleObject1 = new THREE.Mesh(geometry1, material1);
+collectibleObject.position.set(4, 2, -6);
+collectibleObject.name = "CollectibleGeometry1";
+scene.add(collectibleObject1);
 
-const collectible4 = collectibleObject.clone();
-collectible4.position.set(-3, 2, -6);
-collectible4.name = "Collectible4";
-scene.add(collectible4);
-
-const collectible5 = collectibleObject.clone();
-collectible5.position.set(4, 1.2, -10);
-collectible5.name = "Collectible5";
-scene.add(collectible5);
-
+const collectibleObject = new THREE.Mesh(geometry, material);
+collectibleObject.position.set(0, 2, -6);
+collectibleObject.name = "CollectibleGeometry";
+scene.add(collectibleObject);
 
 let lookingAtObject = null;
 let lookStartTime = 0;
@@ -134,6 +130,11 @@ function setupVRControllers() {
         const pickedObject = intersects[0].object;
 
         if (pickedObject.name === "CollectibleGeometry") {
+          window.alert("¡Objeto coleccionable detectado!");
+          pickUpObject(pickedObject);
+          return;
+        } 
+        else if (pickedObject.name === "CollectibleGeometry1") {
           window.alert("¡Objeto coleccionable detectado!");
           pickUpObject(pickedObject);
           return;
